15c5283b783121146e050221445aed92531b1037
gamal <gamal.pratama@scm.co.id>
2016-04-12 14:56:53 +0700

    frontpage
    - add mobile detect for better user-agent detection
    - add 2 new template on header.php; layout-desktop.php & layout-mobile.php

--- backend/wp-content/themes/atvi-theme/header.php
+++ backend/wp-content/themes/atvi-theme/header.php
@@ -25,26 +25,13 @@
 	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'atvi-theme' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+		<?php if(wpmd_is_notdevice()) { 
+			get_template_part('template-parts/layout', 'desktop');
+		} else { 
+			get_template_part('template-parts/layout', 'mobile');
+		} ?>
+		
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'atvi-theme' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
