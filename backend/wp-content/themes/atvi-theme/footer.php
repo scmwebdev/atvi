4205a56f9f4308826f537e0745798db29c547b0d
gamal <gamal.pratama@scm.co.id>
2016-04-14 19:06:34 +0700

    update frontpage template and add new custom field box on it

--- backend/wp-content/themes/atvi-theme/footer.php
+++ backend/wp-content/themes/atvi-theme/footer.php
@@ -14,12 +14,12 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
+	<!-- <div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'atvi-theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'atvi-theme' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'atvi-theme' ), 'atvi-theme', '<a href="http://underscores.me/" rel="designer">gamal</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+		</div>-->
+	</footer> 
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- backend/wp-content/themes/atvi-theme/frontpage.php
+++ backend/wp-content/themes/atvi-theme/frontpage.php
@@ -5,50 +5,50 @@
  * @package atvi
  */
 
-// store the image in a variable
-$slider_1 = get_field("slider_1");
-$slider_1_text = get_field("slider_1_text");
-$slider_2 = get_field("slider_2");
-$slider_2_text = get_field("slider_2_text");
-$slider_3 = get_field("slider_3");
-$slider_3_text = get_field("slider_3_text");
-$slider_4 = get_field("slider_4");
-$slider_4_text = get_field("slider_4_text");
-
 get_header(); ?>
-<div id="primary" class="content-area">
+<div id="primary" class="content-area <?php echo get_the_title() ?>">
 	<main id="main" class="site-main" role="main">
 		<div class="carousel" id="atvi-carousel">
-			<?php if($slider_1) { ?>
-				<div class="carousel-slider-1 ?>">
-					<img src="<?php echo $slider_1 ?>" alt="">
+			<?php for($i = 1; $i <= 4; $i++) { 
+				$slider =  ${'slider_'.$i} = get_field("slider_" . $i);
+				if($slider) {
+			?>
+			<div class="carousel-slider-1 ?>">
+					<img src="<?php echo $slider ?>" alt="">
 				</div>
+
 			<?php } else {
-				echo '';
-			} ?>
-			<?php if($slider_2) { ?>
-				<div class="carousel-slider-2 ?>">
-					<img src="<?php echo $slider_2 ?>" alt="">
+					echo '';
+				} //endof if
+			} //endof for
+			?>
+		</div>
+		<div class="sub __spacepad">
+			<div class="container">
+				<div class="item">
+				<?php for($i = 1; $i <= 6; $i++) { 
+						$tagline =  ${'tagline_'.$i} = get_field("tagline_" . $i);
+						$tagline_desc =  ${'tagline_desc_'.$i} = get_field("tagline_desc_" . $i);
+						$tagline_img =  ${'tagline_img_'.$i} = get_field("tagline_img_" . $i);
+				?>
+					<div class="item-list col-xs-12 col-sm-4 __spacepad">
+						<div class="item-list-container">
+							<div class="item-list-icon">
+								<img class="img-responsive" src="<?php echo $tagline_img; ?>" alt="">
+							</div>
+							<div class="item-list-desc">
+								<div class="item-list-desc-button">
+									<?php echo $tagline; ?>
+								</div>
+								<div class="item-list-desc-text">
+									<?php echo $tagline_desc; ?>
+								</div>
+							</div>
+						</div>
+					</div>
+				<?php } ?>
 				</div>
-			<?php } else {
-				echo '';
-			} ?>
-			<?php if($slider_3) { ?>
-				<div class="carousel-slider-3 ?>">
-					<img src="<?php echo $slider_3 ?>" alt="">
-				</div>
-			<?php } else {
-				echo '';
-			} ?>
-			<?php if($slider_4) { ?>
-				<div class="carousel-slider-4 ?>">
-					<img src="<?php echo $slider_4 ?>" alt="">
-				</div>
-			<?php } else {
-				echo '';
-			} ?>
-			
-				
+			</div>
 		</div>
 	</main><!-- #main -->
 </div><!-- #primary -->
