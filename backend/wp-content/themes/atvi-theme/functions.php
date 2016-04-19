4ca5678c3de9d13902edf44796e0252b28b385c7
gamal <gamal.pratama@scm.co.id>
2016-04-19 14:53:08 +0700

    add new image sizes

--- backend/wp-content/themes/atvi-theme/functions.php
+++ backend/wp-content/themes/atvi-theme/functions.php
@@ -152,3 +152,11 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/* ==================================================================
+ * Additional Image Sizes
+ * ================================================================== */
+
+add_image_size( 'mainBanner_lg', 1920, 600, true);
+add_image_size( 'mainBanner_md', 992, 400, true);
+add_image_size( 'mainBanner_xs', 600, 600, true);
