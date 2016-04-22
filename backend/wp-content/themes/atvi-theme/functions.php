ea87675e2a3c84aa632d30c0f3d006aefa05fa1f
gamal <gamal.pratama@scm.co.id>
2016-04-22 18:29:46 +0700

    update tenaga pengajar page

--- backend/wp-content/themes/atvi-theme/functions.php
+++ backend/wp-content/themes/atvi-theme/functions.php
@@ -235,3 +235,69 @@ function the_slug($echo=true){
   do_action('after_slug', $slug);
   return $slug;
 }
+
+/* ==================================================================
+ * Custom Pagination
+ * ================================================================== */
+
+function custom_pagination($numpages = '', $pagerange = '', $paged='') {
+
+  if (empty($pagerange)) {
+    $pagerange = 2;
+  }
+
+  /**
+   * This first part of our function is a fallback
+   * for custom pagination inside a regular loop that
+   * uses the global $paged and global $wp_query variables.
+   * 
+   * It's good because we can now override default pagination
+   * in our theme, and use this function in default quries
+   * and custom queries.
+   */
+  global $paged;
+  if (empty($paged)) {
+    $paged = 1;
+  }
+  if ($numpages == '') {
+    global $wp_query;
+    $numpages = $wp_query->max_num_pages;
+    if(!$numpages) {
+        $numpages = 1;
+    }
+  }
+
+  /** 
+   * We construct the pagination arguments to enter into our paginate_links
+   * function. 
+   */
+  $pagination_args = array(
+    'base'            => get_pagenum_link(1) . '%_%',
+    'format'          => 'page/%#%',
+    'total'           => $numpages,
+    'current'         => $paged,
+    'show_all'        => False,
+    'end_size'        => 1,
+    'mid_size'        => $pagerange,
+    'prev_next'       => True,
+    'prev_text'       => __('&laquo;'),
+    'next_text'       => __('&raquo;'),
+    'type'            => 'plain',
+    'add_args'        => false,
+    'add_fragment'    => ''
+  );
+
+  $paginate_links = paginate_links($pagination_args);
+
+  if ($paginate_links) {
+    echo '<nav class="custom-pagination col-xs-12 text-center __spacepad">';
+      //echo "<span class='page-numbers page-num'>Page " . $paged . " of " . $numpages . "</span> ";
+      echo $paginate_links;
+    echo "</nav>";
+  }
+
+}
+
+
+
+
--- backend/wp-content/themes/atvi-theme/template-parts/atvi-tenaga-pengajar.php
+++ backend/wp-content/themes/atvi-theme/template-parts/atvi-tenaga-pengajar.php
@@ -1,17 +1,65 @@
-<?php 
-	$photo = get_field('pengajar_img');
-	$name = get_field('pengajar_name');
-	$role = get_field('pengajar_role');
-?>
+<?php
+	/*
+	 * Template for tenaga pengajar
+	 */
 
-<div class="item">
-	<div class="item-list col-xs-12 col-sm-4">
-		<div class="item-list-photo">
-			<img class="img-responsive __fullwidth" src="<?php echo $photo; ?>" alt="<?php echo $name; ?>">
+	$url = site_url();
+
+?>
+<div class="user-content col-xs-12 col-sm-9">
+	<div class="item">
+
+	<?php
+		$paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
+		$args = array(
+			'post_type' => 'post',
+			'category_name' => 'pengajar',
+			'posts_per_page' => 6,
+			'order' => 'DESC',
+			'paged' => $paged
+		);
+
+		$the_query = new WP_Query($args);
+
+		if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); 
+		$role = get_field('role');
+	?>
+
+		<div class="item-list col-xs-12 __nopadding">
+			<div class="item-list-photo col-xs-12 col-sm-4">
+				<?php 
+					if(has_post_thumbnail()) {
+						the_post_thumbnail(); 
+					} else { ?>
+						<img class="img-responsive" src="<?php echo $url ?>/wp-content/uploads/2016/04/person_nothumbnail.png" alt="no thumbnail">
+					<?php }
+				?>
+			</div>
+			<div class="item-list-desc col-xs-12 col-sm-8">
+				<div class="item-list-name"><?php the_title(); ?></div>
+				<!-- <div class="item-list-role"><?php echo $role; ?></div> -->
+				<div class="item-list-exp __spacepad"><?php the_content(); ?></div>
+			</div>
 		</div>
-		<div class="item-list-desc __spacepad">
-			<div class="item-list-name"><?php echo $name; ?><div>
-			<div class="item-list-role"><?php echo $role; ?><div>
-		</div>
+	<?php 
+		endwhile;
+			if (function_exists(custom_pagination)) {
+				custom_pagination($the_query->max_num_pages,"",$paged);
+			} else {
+				echo 'function does not exist!';
+			}
+		else:
+	?>
+	<article>
+		<h1>Sorry...</h1>
+		<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+	</article>
+
+	<?php 
+		endif;
+		wp_reset_postdata();
+	
+	?>
+
 	</div>
 </div>
