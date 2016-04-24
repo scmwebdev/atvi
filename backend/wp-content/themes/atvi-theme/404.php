726925a68f120275e457cce95598dabe1009799e
gamal <gamal.pratama@scm.co.id>
2016-04-24 15:19:54 +0700

    update 404 page

--- backend/wp-content/themes/atvi-theme/404.php
+++ backend/wp-content/themes/atvi-theme/404.php
@@ -11,52 +11,31 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+		<section class="__fullsegment __relative">
+			<div class="container">
+				<section class="error-404 not-found ">
+					<header class="page-header __spacepad clearfix vcenter">
+						<div class="col-xs-12 col-sm-3 text-center">
+							<i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+						</div>
+						<div class="col-xs-12 col-sm-9">
+							<h1 class=" page-title"><?php esc_html_e( 'Uh oh! Page yang anda cari tidak ada', 'atvi-theme' ); ?></h1>
+							<p>Pastikan anda menulis URL dengan benar atau click button dibawah untuk kembali ke home atau kembali ke halaman sebelumnya</p>
+							<div class="error-404-btn">
+								<a title="Kembali ke halaman sebelumnya">
+									<button type="button" class="btn btn-primary back-btn">Back</button>
+								</a>
+								<a href="<?php echo get_home_url(); ?>" title="Kembali ke homepage">
+									<button type="button" class="btn btn-primary">Home</button>
+								</a>
+							</div>
+						</div>
+						
+					</header><!-- .page-header -->
+				</section><!-- .error-404 -->
+			</div>
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'atvi-theme' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'atvi-theme' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( atvi_theme_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'atvi-theme' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'atvi-theme' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
+		</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
