208e2ee95705992c72299a039577a13dd8ba0ab7
gamal <gamal.pratama@scm.co.id>
2016-04-28 11:01:12 +0700

    upload some missing core plugins files

---
+++ 404_original/404-page.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>ATVI | Under Construction</title>
+
+		<!-- Main CSS -->
+		<link rel="stylesheet" href="main.min.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div class="page" id="under-construction">
+				<img class="img-responsive __fullwidth" src="inc/image/under-construction_new.jpg" alt="page - under construction">	
+
+		</div>
+		<style type="text/css" media="screen">
+			#under-construction{
+				position: relative;
+				background-color: #F0F0F0;
+			}
+		</style>
+	</body>
+</html>
