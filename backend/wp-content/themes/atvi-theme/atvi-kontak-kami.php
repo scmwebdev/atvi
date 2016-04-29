97af7ae6380c1b52308c45e48c4f7ca32c4648dc
gamal <gamal.pratama@scm.co.id>
2016-04-29 19:08:18 +0700

    update kontak kami page

---
+++ backend/wp-content/themes/atvi-theme/atvi-kontak-kami.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Template for display the frontpage of the website
+ * Template Name: Atvi Kontak Kami Page 
+ * @package atvi
+ */
+
+get_header(); ?>
+<div id="primary" class="content-area atvi-contentpage <?php echo get_the_title() ?>">
+	<main id="main" class="site-main clearfix" role="main">
+		<div class="gmap">
+			<?php
+				$gmap = get_field('google_map');
+				echo '<div class="overlay" onClick="style.pointerEvents=\'none\'"></div>';
+				echo $gmap;
+			?>
+		</div>
+			<div class="page-content">
+				<?php if(wpmd_is_notphone()) { ?>
+				<div class="container">
+					<?php if ( function_exists('yoast_breadcrumb') ) 
+						{yoast_breadcrumb('<div class="small" id="breadcrumbs"><p class="breadcrumbs-content">','</p></div>');} 
+					?>
+					<?php 
+						//get_template_part('template-parts/atvi', 'pagecontent'); 
+					?>
+					<div class="user-content">
+						<h1 class="title"><?php the_title() ?></h1>
+						<hr>
+						<?php the_content(); ?>
+					</div>
+				</div>
+				<?php } else { 
+				
+					get_template_part('template-parts/atvi', 'pagecontent');
+				}
+				?>
+			</div>
+		</div>
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
--- backend/wp-content/themes/atvi-theme/page.php
+++ backend/wp-content/themes/atvi-theme/page.php
@@ -16,16 +16,11 @@ get_header(); ?>
 
 	<div id="primary" class="content-area rte">
 		<main id="main" class="site-main" role="main">
-
 			<?php
-
-			while ( have_posts() ) : the_post();
-				
-				get_template_part( 'template-parts/content', 'page' );
-							
-			endwhile; // End of the loop.
+				while ( have_posts() ) : the_post();
+					get_template_part( 'template-parts/content', 'page' );		
+				endwhile; // End of the loop.
 			?>
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
