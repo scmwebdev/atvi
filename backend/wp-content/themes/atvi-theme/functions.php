65d782c4eafb0855331f2062879be0c86f88c6e9
gamal <gamal.pratama@scm.co.id>
2016-06-10 13:47:17 +0700

    fix childmenu styling and spacing

--- backend/wp-content/themes/atvi-theme/functions.php
+++ backend/wp-content/themes/atvi-theme/functions.php
@@ -216,7 +216,7 @@ if ( is_page() && $post->post_parent )
 else
   $childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0' );
 if ( $childpages ) {
-  $string = '<ul class="content-list list-parent __nodots __nopaddingleft __normarginleft">' . $childpages . '</ul>';
+  $string = '<ul class="content-list list-parent nodots no-padding-left no-margin-left">' . $childpages . '</ul>';
 }
 return $string;
 }
